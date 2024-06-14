24533cfd1cdf7d2344b43eec3cb7dc84f6f1f0a0
Danilo Feijó <danilo.silvafs@gmail.com>
2024-06-14 16:55:45 -0300

    build: create method to generate password

--- utils/utils.ts
+++ utils/utils.ts
@@ -4,25 +4,38 @@
 
 import { faker } from '@faker-js/faker';
 
-class Utils {
+export class Utils {
   /**
-   * Method to generate a random name using faker
+   * Method to generate a random name
    * @returns {string} A random full name
   */
-  public static setRandomName(): string {
+  public static setFullName(): string {
     return faker.person.fullName()
   }
 
   /**
-   * Method to generate a random email using faker
+   * Method to generate a random email
    * @param {string} [alias] - Optional alias for generating email
    * @returns {string} A random email address
    */
-  public static setRandomEmail(alias?: string): string {
+  public static setEmail(alias?: string): string {
     if (!alias) {
-      alias = this.setRandomName()
+      alias = this.setFullName()
     }
     return `${alias.toLowerCase().replace(/\s+/g, '')}@pwlab-feijo.com`
+  }
+
+  /**
+   * Method to generate a random password
+   * @returns {string} A randon password
+   */
+  public static setPassword(): string {
+    return faker.internet.password({
+      length: 12
+      // pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,12}$/
+        // Have between 8 and 12 characters being
+        // Uppercase, lowercase, number and special
+    })
   }
 }
 
