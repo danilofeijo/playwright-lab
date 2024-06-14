b44670648129c94049e7bf3cc823b86ef77d55da
Danilo Feijó <danilo.silvafs@gmail.com>
2024-06-14 16:57:45 -0300

    refactor: dont repeat yourself

--- e2e/signup.spec.ts
+++ e2e/signup.spec.ts
@@ -1,82 +1,70 @@
 import { test, expect } from '@playwright/test'
-
-import Utils from '../utils/utils'
-import { faker } from '@faker-js/faker'
+import { Utils } from '../utils/utils'
 
 let USER: { nome: string; email: string; pass: string }
 
-test('Should create a new admin user', async ({ page }) => {
-  // Arrange
-  const nome = Utils.setRandomName()
-  const email = Utils.setRandomEmail(nome)
+test.describe('On Signup page', () => {
+  test.beforeEach(async ({page}) => {
+    const userName = Utils.setFullName()
 
-  USER = {
-    nome,
-    email,
-    pass: `Test;123`,
-  };
+    USER = {
+      nome: userName,
+      email: Utils.setEmail(userName),
+      pass: Utils.setPassword()
+    };
 
-  await page.goto('/cadastrarusuarios')
+    await page.goto('/cadastrarusuarios')
+  });
 
-  // Act
-  await page.getByTestId('nome').fill(USER.nome)
-  await page.getByTestId('email').fill(USER.email)
-  await page.getByTestId('password').fill(USER.pass)
-  await page.getByTestId('checkbox').click()
-  await page.locator('data-testid=cadastrar').click()
+  test('Should create a new admin user', async ({ page }) => {
+    // Act
+    await page.getByTestId('nome').fill(USER.nome)
+    await page.getByTestId('email').fill(USER.email)
+    await page.getByTestId('password').fill(USER.pass)
+    await page.getByTestId('checkbox').click()
+    await page.locator('data-testid=cadastrar').click()
 
-  // Assert
-  await expect(page).toHaveURL('/admin/home')
-  await expect(page.locator('h1')).toBeVisible()
-  await expect(page.locator('h1')).not.toBeHidden()
-  await expect(page.locator('h1')).toContainText(USER.nome)
+    // Assert
+    await expect(page).toHaveURL('/admin/home')
+    await expect(page.locator('h1')).toBeVisible()
+    await expect(page.locator('h1')).not.toBeHidden()
+    await expect(page.locator('h1')).toContainText(USER.nome)
 
-  // TODO - Develop assertions
-  // await expect(menuCadastrarUsuarios).toHaveText('Cadastrar Usuários')
-  // await expect(menuListarUsuarios).toHaveText('Listar Usuários')
-  // await expect(menuCadastrarProdutos).toHaveText('Cadastrar Produtos')
-  // await expect(menuListarProdutos).toHaveText('Listar Produtos')
-  // await expect(menuRelatorios).toHaveText('Relatórios')
-  // await expect(buttonLogout).toBeVisible()
+    // TODO - Develop assertions
+    // await expect(menuCadastrarUsuarios).toHaveText('Cadastrar Usuários')
+    // await expect(menuListarUsuarios).toHaveText('Listar Usuários')
+    // await expect(menuCadastrarProdutos).toHaveText('Cadastrar Produtos')
+    // await expect(menuListarProdutos).toHaveText('Listar Produtos')
+    // await expect(menuRelatorios).toHaveText('Relatórios')
+    // await expect(buttonLogout).toBeVisible()
 
-  // await expect(menuListaCompras).not.toBeVisible() // or .toBeHidden()
-  // await expect(menuCarrinho).not.toBeVisible()
-});
+    // await expect(menuListaCompras).not.toBeVisible() // or .toBeHidden()
+    // await expect(menuCarrinho).not.toBeVisible()
+  });
 
-test('Should create a common user',  async ({ page }) => {
-  // Arrange
-  const nome = Utils.setRandomName()
-  const email = Utils.setRandomEmail(nome)
+  test('Should create a common user',  async ({ page }) => {
+    // Act
+    await page.getByTestId('nome').fill(USER.nome)
+    await page.getByTestId('email').fill(USER.email)
+    await page.getByTestId('password').fill(USER.pass)
+    await page.locator('data-testid=cadastrar').click()
 
-  USER = {
-    nome,
-    email,
-    pass: `Test;123`,
-  };
+    // Assert
+    await expect(page).toHaveURL('/home')
+    await expect(page).not.toHaveURL('/admin/home')
+    await expect(page.locator('h1')).toBeVisible()
+    await expect(page.locator('h1')).toContainText('Serverest Store')
 
-  await page.goto('/cadastrarusuarios')
+    // TODO - Develop assertions
+    // await expect(menuListaCompras).toHaveText('Lista de Compras')
+    // await expect(menuCarrinho).toHaveText('Carrinho')
+    // await expect(buttonLogout).toBeVisible()
+    // await expect(buttonLogout).toBeVisible()
 
-  // Act
-  await page.getByTestId('nome').fill(USER.nome)
-  await page.getByTestId('email').fill(USER.email)
-  await page.getByTestId('password').fill(USER.pass)
-  await page.locator('data-testid=cadastrar').click()
-
-  // Assert
-  await expect(page).toHaveURL('/home')
-  await expect(page).not.toHaveURL('/admin/home')
-  await expect(page.locator('h1')).toBeVisible()
-  await expect(page.locator('h1')).toContainText('Serverest Store')
-
-  // TODO - Develop assertions
-  // await expect(menuListaCompras).toHaveText('Lista de Compras')
-  // await expect(menuCarrinho).toHaveText('Carrinho')
-  // await expect(buttonLogout).toBeVisible()
-  // await expect(buttonLogout).toBeVisible()
-
-  // await expect (menuCadastrarUsuarios).toBeHidden()
-  // await expect (menuListarUsuarios).toBeHidden()
-  // await expect (menuCadastrarProdutos).toBeHidden()
-  // await expect (menuListarProdutos).toBeHidden()
-  // await expect (menuRelatorios).toBeHidden()
-});
+    // await expect (menuCadastrarUsuarios).toBeHidden()
+    // await expect (menuListarUsuarios).toBeHidden()
+    // await expect (menuCadastrarProdutos).toBeHidden()
+    // await expect (menuListarProdutos).toBeHidden()
+    // await expect (menuRelatorios).toBeHidden()
+  });
+})
