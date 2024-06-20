555b46ab4e1e8892b400bdb0f199f98634c00c6f
Danilo Feijó <danilo.silvafs@gmail.com>
2024-06-20 18:18:05 -0300

    test: map scenario to develop

---
+++ e2e/login.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('On login page', () => {
+  test.describe('as Admin user', () => {
+    test('Should log in with Admin user credentials', async ({ page }) => {
+      // TODO - Develop test scenario
+      // Arrange
+      // Act
+      // Assert
+    })
+
+    test('Should not log in with wrong credentials', async ({ page }) => {
+      // TODO - Develop test scenario
+      // Arrange
+      // Act
+      // Assert
+    })
+  })
+
+  test.describe('as Common user', () => {})
+})
