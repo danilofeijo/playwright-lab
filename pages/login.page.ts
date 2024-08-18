aee591cc3721f2ce4169d0a87279a63c5e24f576
Danilo Feijó <danilo.silvafs@gmail.com>
2024-08-17 21:15:09 -0300

    fix: rename variables

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -4,17 +4,17 @@ import { GlobalPage } from '../pages/global.page'
 export class LoginPage extends GlobalPage {
   // Variables
   readonly urlPath: string
-  readonly emailField: Locator
-  readonly passwordField: Locator
-  readonly entrarButton: Locator
+  readonly fieldEmail: Locator
+  readonly fieldPassword: Locator
+  readonly buttonEntrar: Locator
   
   // Constructor
   constructor(page: Page) {
     super(page)
     this.urlPath = '/login'
-    this.emailField = page.getByTestId('email')
-    this.passwordField = page.getByTestId('senha')
-    this.entrarButton = page.getByTestId('entrar')
+    this.fieldEmail = page.getByTestId('email')
+    this.fieldPassword = page.getByTestId('senha')
+    this.buttonEntrar = page.getByTestId('entrar')
   }
 
   // Methods
