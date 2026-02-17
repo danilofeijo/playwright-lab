800484b6891a580b482b2edb7ede595ccbd4fef4
Danilo Feijó <danilo.silvafs@gmail.com>
2026-02-17 18:39:10 -0300

    feat: mock 500 error

---
+++ e2e/mocks.ts
@@ -0,0 +1,19 @@
+import { Route } from '@playwright/test'
+
+export const serverestMocks = {
+  error500: async (route: Route) => {
+    route.fulfill({
+      status: 500,
+      body: 'Internal Server Error'
+    })
+  },
+
+  // GET /produtos 200
+  // produtos: (route: Route) => route.fulfill({
+  //   status: 200,
+  //   json: [
+  //     { id: 1, nome: 'Produto Mock 1', preco: 10.99 },
+  //     { id: 2, nome: 'Produto Mock 2', preco: 25.50 }
+  //   ]
+  // }),
+}
